bdc41320dfcba13ee15b1f63b7928eae2fb44f3e
Shubham Mehta <shubham@Shubhams-MacBook-Air.local>
2026-03-13 18:11:41 +0530

    fix urlRoutes import

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const urlRoutes = require("./routes/urlRoutes");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("URL Shortener API is running 🚀");
+  res.send("URL Shortener API running 🚀");
 });
 
 app.use("/api", urlRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
