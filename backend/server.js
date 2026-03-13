10b7797d98c8c15e03a5f9101b373dd7cc497306
Shubham Mehta <shubham@Shubhams-MacBook-Air.local>
2026-03-13 17:59:40 +0530

    fix root route

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,14 @@
-require("dotenv").config();
-
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const urlRoutes = require("./routes/urlRoutes");
-
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Atlas Connected"))
-.catch(err => console.log(err));
+app.get("/", (req, res) => {
+  res.send("URL Shortener API is running 🚀");
+});
 
 app.use("/api", urlRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server running");
 });
